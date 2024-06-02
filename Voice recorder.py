1b41ecf0c35c1d3b9d9c05ec9a56fefe85fb42a3
ad-astra1111 <pompouspapli@gmail.com>
2024-06-03 03:34:39 +0530

    Add files via upload

---
+++ Voice recorder.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 03:29:25 2024
+
+@author: AARATRIKA
+"""
+
+# import required libraries
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as wv
+ 
+# Sampling frequency
+freq = 44100
+ 
+# Recording duration
+duration = 5
+ 
+# Start recorder with the given values 
+# of duration and sample frequency
+recording = sd.rec(int(duration * freq), 
+                   samplerate=freq, channels=2)
+ 
+# Record audio for the given number of seconds
+sd.wait()
+ 
+# This will convert the NumPy array to an audio
+# file with the given sampling frequency
+write("recording0.wav", freq, recording)
+ 
+# Convert the NumPy array to audio file
+wv.write("recording1.wav", recording, freq, sampwidth=2)
